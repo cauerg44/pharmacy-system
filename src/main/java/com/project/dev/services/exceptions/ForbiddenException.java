fe4fcc835b0ce2c0f398d154b0a23c58ec64fdc4
Caue Garcia <cauerg7@outlook.com>
2024-03-27 00:34:07 -0300

    customizing validation errors.

---
+++ src/main/java/com/project/dev/services/exceptions/ForbiddenException.java
@@ -0,0 +1,8 @@
+package com.project.dev.services.exceptions;
+
+public class ForbiddenException extends RuntimeException {
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
